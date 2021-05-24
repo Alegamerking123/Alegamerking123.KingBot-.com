31cc1eb4eb61d421566b06f42edb41dd96b7b277
Alegamerking123 <alejandromartinez31032006@gmail.com>
2021-05-24 18:50:27 -0400

    WebKB

---
+++ WebKingBot/Paginas/main.js
@@ -0,0 +1,11 @@
+console.log("KKKKK    KKKKK    BBBBBBBBBB       K        KK        K\nKKKKK   KKKKK     BBBBB  BBBBB     KK      KKKK      KK\nKKKKK  KKKKK      BBBBB  BBBBB     KKK    KKKKKK    KKK\nKKKKK KKKKK       BBBBBBBBBB       KKKKKKKKKKKKKKKKKKKK\nKKKKK KKKKK       BBBBB  BBBBB     KKKKKKKKKKKKKKKKKKKK\nKKKKK  KKKKK      BBBBB   BBBBB    KKKKKKKKKKKKKKKKKKKK\nKKKKK   KKKKK     BBBBB    BBBBB   KKKKKKKKKKKKKKKKKKKK\nKKKKK    KKKKK    BBBBB    BBBBB   KKKKKKKKKKKKKKKKKKKK\nKKKKK     KKKKK   BBBBBBBBBBBB     KKKKKKKKKKKKKKKKKKKK\n");
+
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$(".menu").click(function(){
+		$(".Navegador-2").toggle();
+	});
+};
